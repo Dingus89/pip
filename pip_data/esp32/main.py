1164c7582b70342bfd22cb9d64d265de558b85d3
Dingus89 <khoward4171@gmail.com>
2025-05-08 17:43:13 -0500

    Update main.py

--- pip_data/esp32/main.py
+++ pip_data/esp32/main.py
@@ -9,6 +9,31 @@ import _thread
 from esp32_motor_ir import send_ir_command
 from sensor_handlers import read_pir, read_ultrasonic
 from capacitance_sensor import read_touch
+from machine import Pin, UART
+
+
+uart = UART(1, tx=19, rx=20, baudrate=115200)
+
+pir_sensor = Pin(10, Pin.IN)
+
+red = Pin(5, Pin.OUT)
+green = Pin(6, Pin.OUT)
+blue = Pin(7, Pin.OUT)
+
+def set_rgb(r, g, b):
+    red.value(r)
+    green.value(g)
+    blue.value(b)
+
+print("ESP32 ready...")
+
+while True:
+    if pir_sensor.value():
+        uart.write("motion_detected\n")
+        set_rgb(1, 0, 0)
+        time.sleep(1)
+        set_rgb(0, 1, 0)
+    time.sleep(0.1)
 
 WIFI_SSID = "2c7b65"
 WIFI_PASS = "rating.163.prevail"

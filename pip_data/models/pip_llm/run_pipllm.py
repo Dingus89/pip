412bbe5cf6b98f2896355b9304122f03f9ba3337
Dingus89 <khoward4171@gmail.com>
2025-05-17 01:42:39 -0500

    Update run_pipllm.py

--- pip_data/models/pip_llm/run_pipllm.py
+++ pip_data/models/pip_llm/run_pipllm.py
@@ -1,15 +1,18 @@
 from pipeline import TinyLLM
+from pip_ai import get_response
 
 llm = TinyLLM()
 
-print("Pip is ready. Type something!")
+print("Pip is online!")
 
 while True:
     try:
-        user_input = input("You: ").strip()
-        if user_input.lower() in {"exit", "quit"}:
+        user_input = input("You: ")
+        if user_input.strip().lower() in ["exit", "quit"]:
             break
-        response = llm.process(user_input)
-        print("Pip:", response)
+
+        reply = get_response(user_input)
+        print("Pip:", reply)
+
     except KeyboardInterrupt:
         break
